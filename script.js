2cc654047d9580d8d27356807b3c29270baccb38
Ian Quinto <codedianq@gmail.com>
2023-09-09 17:51:04 +0800

    Add button to generate grid depends on the user.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 //variables
-// let gridNum = prompt('Input number for rows and columns:');
+let gridSize = 0;
 
 //reference for the sketchpad
 const container = document.querySelector('#container');
 let gridRow = document.getElementsByClassName('rows');
 let gridCol = document.getElementsByClassName('cols');
 
+//reference for the controls
+const controls = document.querySelector('#controls');
+const pickSize = document.querySelector('.size-btn');
+
 //create a function that will generate the sketchpad grid
+function gridNum() {
+    for(let i = 0; i < gridRow.length; i++) {
+        for(let j = 0; j < gridCol.length; j++) {
+            container.removeChild(gridRow[i])
+        }
+    }
+    gridSize = prompt('Pick your grid size between 1 - 100 only', '16');
+    let gridSizeInt = parseInt(gridSize)
+    if(gridSizeInt > 100) {
+        alert(`${gridSizeInt} is not Valid. Choose between 1 - 100 only`)
+        defaultGrid()
+    } else if(gridSizeInt < 0) {
+        alert(`${gridSizeInt} is not Valid. Choose between 1 - 100 only`)
+        defaultGrid()
+    } else {
+        createRow(gridSize);
+        createCol(gridSize);
+    }
+    
+}
+
+//button for generating grids
+pickSize.addEventListener('click', (gridNum))
+
 function defaultGrid() {
     createRow(16);
     createCol(16);
@@ -32,7 +60,6 @@ function createCol(numCol) {
         
     }
 }
-
 
 defaultGrid()
 
